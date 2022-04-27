1629a008bf1bdbec0352dc4a4872f18e0bad3f17
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-27 12:51:16 +0530

    returning html pages

--- client-server/server.js
+++ client-server/server.js
@@ -1,15 +1,25 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // console.log("request made");
-  console.log(req.url,req.method)
+  console.log(req.url, req.method);
 
-  res.setHeader('Content-Type', 'text/html')
+  res.setHeader("Content-Type", "text/html");
 
-  res.write("<h3>Hello, Browser!</h3>")
-  res.end();
+  //send html files back as response
+  fs.readFile("./view/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.write(data);
+      res.end();
+
+      //shortcut if only single data => res.end(data)
+    }
+  });
 });
 
-server.listen(3001,'localhost',() =>{
-    console.log("requesting on server 3001")
+server.listen(3001, "localhost", () => {
+  console.log("requesting on server 3001");
 });
