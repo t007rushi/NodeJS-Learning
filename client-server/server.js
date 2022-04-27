3fcb2907079e70ad709c7a29946129274ed8be97
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-27 13:44:42 +0530

    redirection

--- client-server/server.js
+++ client-server/server.js
@@ -5,18 +5,23 @@ const server = http.createServer((req, res) => {
   // console.log("request made");
   console.log(req.url, req.method);
   res.setHeader("Content-Type", "text/html");
-  var path = ""
+  var path = "";
   switch (req.url) {
     case "/":
-      path = "index.html"
+      path = "index.html";
       res.statusCode = 200;
-      break
+      break;
     case "/about":
-      path = "about.html"
+      path = "about.html";
       res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end()
       break
     default:
-      path = "404.html"
+      path = "404.html";
       res.statusCode = 404;
   }
 
