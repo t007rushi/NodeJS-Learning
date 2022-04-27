b9c96bc6484dd0555f2a95fe318a0668ca4d3589
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-27 12:41:34 +0530

    response object

--- client-server/server.js
+++ client-server/server.js
@@ -3,6 +3,11 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   // console.log("request made");
   console.log(req.url,req.method)
+
+  res.setHeader('Content-Type', 'text/html')
+
+  res.write("<h3>Hello, Browser!</h3>")
+  res.end();
 });
 
 server.listen(3001,'localhost',() =>{
